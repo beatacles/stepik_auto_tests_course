5963e411f2215984a58c2daa4abadec1b6dd25e5
beatacles <iam.dkalugin@gmail.com>
2023-05-26 10:49:02 +0300

    add plugins for rerun/flaky tests

---
+++ lesson_3_6_step_8_plugins.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+def test_guest_should_see_login_link_pass(browser):
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+def test_guest_should_see_login_link_fail(browser):
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, "#magic_link")
+
+#pytest -v --tb=line --reruns 1 --browser_name=chrome lesson_3_6_step_8_plugins.py
