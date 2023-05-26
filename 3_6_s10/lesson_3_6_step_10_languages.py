3d59ed46f8cdc2182ddf8c84d60b8d9579f3716a
beatacles <iam.dkalugin@gmail.com>
2023-05-26 11:49:31 +0300

    fix exercise 3.6.10

--- 3_6_s10/lesson_3_6_step_10_languages.py
+++ 3_6_s10/lesson_3_6_step_10_languages.py
@@ -1,7 +1,5 @@
 import time
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
@@ -9,4 +7,4 @@ def test_exist_add_to_cart_button(browser):
     browser.get(link)
     time.sleep(30)
     browser.implicitly_wait(5)
-    assert len(browser.find_elements(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")) == 1, "'Add to basket' button not found!"
+    assert len(browser.find_elements(By.CSS_SELECTOR,".btn.btn-lg.btn-primary.btn-add-to-basket")) == 1, "'Add to basket' button not found!"
